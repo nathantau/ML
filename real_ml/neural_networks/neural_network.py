c5489b42e6549b0b4315e5d92c7896c0aed1dec9
nathan <nathan.au@kooltra.com>
2019-11-08 21:44:44 -0500

    Restructuring and added nn

--- real_ml/neural_networks/neural_network.py
+++ real_ml/neural_networks/neural_network.py
@@ -1,8 +1,18 @@
 import numpy as np
 import math
 
-# let's try to approximate y = x^2 + 5
+def sigmoid(x):
+    return 1/(1 + math.exp(-x))
 
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+# let's try to approximate z = x^2 + 5y + 4
+
+input1 = np.array(2)
+input2 = np.array(3)
+
+# expected output = 23
 
 # 3 layer neural network (1 input 1 hidden 1 output)
 # 2 nodes in each layer
@@ -14,11 +24,13 @@ h2 = np.random.rand(1)
 o1 = np.random.rand(1)
 o2 = np.random.rand(1)
 
-def sigmoid(x):
-    return 1/(1 + math.exp(-x))
+# for now let's not use any biases
 
-def sigmoid_derivative(x):
-    return sigmoid(x) * (1 - sigmoid(x))
+for epoch in range(100):
+
+    # forward pass
+
+    output = (input1 * i1)
 
 
 
